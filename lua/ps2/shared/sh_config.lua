6e0ff3d3b4a8fd7f08343574682db39e6445e311
Sno <pepsiv6@gmail.com>
2017-06-07 15:56:44 -0400

    Upload with folders for easy installation.

---
+++ lua/ps2/shared/sh_config.lua
@@ -0,0 +1,9 @@
+Pointshop2.Config = {}
+local Config = Pointshop2.Config
+
+/*
+	Skin to use
+*/
+Config.DermaSkin = "PS2BlurUI"
+
+if SERVER then resource.AddFile("resource/fonts/gidole-regular.ttf") end
