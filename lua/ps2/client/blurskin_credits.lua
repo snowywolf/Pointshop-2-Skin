6d16785e5dcc9073f3676d517bbcf9b4fff80c58
Valentin Funk <funk.valentin@gmail.com>
2017-06-28 15:59:31 +0200

    Add a credits section

---
+++ lua/ps2/client/blurskin_credits.lua
@@ -0,0 +1,3 @@
+hook.Add( "PS2_PopulateCredits", "AddSkinCreds", function( panel ) 
+    panel:AddCreditSection( "Blur Skin", [[ Blur Skin by Snowywolf (https://github.com/snowywolf/Pointshop-2-Skin). ]] ) 
+end )
